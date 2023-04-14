db534ff6c3493461520445ef08efbb756fb42d88
Monaco <marcelo.monaco@gmail.com>
2023-04-14 09:16:59 -0300

    Renamed client for testing

--- webflow_test.go
+++ webflow_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func init() {
-	client = NewClient(
+	testClient = NewClient(
 		testToken,
 	)
 }
 
 var (
-	client *Client
+	testClient *Client
 )
 
 const (
@@ -26,7 +26,7 @@ const (
 )
 
 func TestAuthorizedUser(t *testing.T) {
-	user, err := client.AuthorizedUser()
+	user, err := testClient.AuthorizedUser()
 	spew.Dump(user)
 	assert.NoError(t, err)
 	assert.NotNil(t, user)
@@ -34,49 +34,49 @@ func TestAuthorizedUser(t *testing.T) {
 }
 
 func TestAuthorizedInfo(t *testing.T) {
-	info, err := client.AuthorizedInfo()
+	info, err := testClient.AuthorizedInfo()
 	spew.Dump(info)
 	assert.NoError(t, err)
 	assert.NotNil(t, info)
 }
 
 func TestListSites(t *testing.T) {
-	sites, err := client.ListSites()
+	sites, err := testClient.ListSites()
 	spew.Dump(sites)
 	assert.NoError(t, err)
 	assert.Greater(t, len(sites), 0)
 }
 
 func TestGetSite(t *testing.T) {
-	site, err := client.GetSite(testSiteId)
+	site, err := testClient.GetSite(testSiteId)
 	assert.NoError(t, err)
 	assert.NotNil(t, site)
 	assert.Equal(t, testSiteId, site.Id)
 }
 
 func TestPublishSite(t *testing.T) {
-	queue, err := client.PublishSite(testSiteId, []string{testDomain})
+	queue, err := testClient.PublishSite(testSiteId, []string{testDomain})
 	spew.Dump(queue)
 	assert.NoError(t, err)
 	assert.Equal(t, queue.Queued, true)
 }
 
 func TestListDomains(t *testing.T) {
-	domains, err := client.ListDomains(testSiteId)
+	domains, err := testClient.ListDomains(testSiteId)
 	spew.Dump(domains)
 	assert.NoError(t, err)
 	assert.Greater(t, len(domains), 0)
 }
 
 func TestListCollections(t *testing.T) {
-	collections, err := client.ListCollections(testSiteId)
+	collections, err := testClient.ListCollections(testSiteId)
 	spew.Dump(collections)
 	assert.NoError(t, err)
 	assert.Greater(t, len(collections), 0)
 }
 
 func TestGetCollection(t *testing.T) {
-	collection, err := client.GetCollection(testCollectionId)
+	collection, err := testClient.GetCollection(testCollectionId)
 	spew.Dump(collection)
 	assert.NoError(t, err)
 	assert.NotNil(t, collection)
@@ -84,14 +84,14 @@ func TestGetCollection(t *testing.T) {
 }
 
 func TestListItems(t *testing.T) {
-	resp, err := client.ListItems(testCollectionId, 0, 10)
+	resp, err := testClient.ListItems(testCollectionId, 0, 10)
 	spew.Dump(resp)
 	assert.NoError(t, err)
 	assert.Greater(t, len(resp.Items), 0)
 }
 
 func TestGetItem(t *testing.T) {
-	item, err := client.GetItem(testCollectionId, testItemId)
+	item, err := testClient.GetItem(testCollectionId, testItemId)
 	spew.Dump(item)
 	assert.NoError(t, err)
 	assert.NotNil(t, item)
@@ -108,7 +108,7 @@ func TestCreateItem(t *testing.T) {
 			"_draft":    true,
 		},
 	}
-	item, err := client.CreateItem(testCollectionId, true, req)
+	item, err := testClient.CreateItem(testCollectionId, true, req)
 	spew.Dump(item)
 	assert.NoError(t, err)
 	assert.NotNil(t, item)
@@ -124,14 +124,14 @@ func TestUpdateItem(t *testing.T) {
 			"_draft":    true,
 		},
 	}
-	item, err := client.UpdateItem(testCollectionId, testItemId, true, req)
+	item, err := testClient.UpdateItem(testCollectionId, testItemId, true, req)
 	spew.Dump(item)
 	assert.NoError(t, err)
 	assert.NotNil(t, item)
 }
 
 func TestRemoveItem(t *testing.T) {
-	resp, err := client.RemoveItem(testCollectionId, testItemId, false)
+	resp, err := testClient.RemoveItem(testCollectionId, testItemId, false)
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Equal(t, 1, resp.Deleted)
